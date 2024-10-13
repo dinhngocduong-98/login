4c104cc97c5ff2a1460a437b0eaae202f599ef39
dinhduongnb98 <dinhduongnb98@gmail.com>
2024-10-13 18:34:09 +0700

    comit data

--- mocks/data/question1.js
+++ mocks/data/question1.js
@@ -338,5 +338,600 @@ export default  [
         ],
         "answer": "at",
         "selectedAnswer": null
+    },
+    {
+        "id": 5021,
+        "question": "_______ did you travel to Da Nang? - By plane.",
+        "plans": [
+            {
+                "value": "How"
+            },
+            {
+                "value": "What"
+            },
+            {
+                "value": "When"
+            }
+        ],
+        "answer": "How",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5022,
+        "question": "My brother ________ pilot.",
+        "plans": [
+            {
+                "value": "is a"
+            },
+            {
+                "value": "are"
+            },
+            {
+                "value": "do"
+            }
+        ],
+        "answer": "is a",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5023,
+        "question": "We are too late. The plane_______off ten minutes ago",
+        "plans": [
+            {
+                "value": "take"
+            },
+            {
+                "value": "takes"
+            },
+            {
+                "value": "took"
+            }
+        ],
+        "answer": "took",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5024,
+        "question": "Our company is______ Vo Nguyen Giap Street",
+        "plans": [
+            {
+                "value": "in"
+            },
+            {
+                "value": "on"
+            },
+            {
+                "value": "at"
+            }
+        ],
+        "answer": "on",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5025,
+        "question": "An airplane travels a distance of 1800km in 2 hours. The speed of the airplane is_____",
+        "plans": [
+            {
+                "value": "700 km/h"
+            },
+            {
+                "value": "600 km/h"
+            },
+            {
+                "value": "900 km/h"
+            }
+        ],
+        "answer": "900 km/h",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5026,
+        "question": "He always ______delicious meals",
+        "plans": [
+            {
+                "value": "make"
+            },
+            {
+                "value": "makes"
+            },
+            {
+                "value": "making"
+            }
+        ],
+        "answer": "makes",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5027,
+        "question": "Can you fix ________ computer ?",
+        "plans": [
+            {
+                "value": "an"
+            },
+            {
+                "value": "this"
+            },
+            {
+                "value": "these"
+            }
+        ],
+        "answer": "this",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5028,
+        "question": "My father is _____ some plants in the garden.",
+        "plans": [
+            {
+                "value": "watering"
+            },
+            {
+                "value": "water"
+            },
+            {
+                "value": "had water"
+            }
+        ],
+        "answer": "watering",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5029,
+        "question": "She’s talking to you. Please listen to ________.",
+        "plans": [
+            {
+                "value": "he"
+            },
+            {
+                "value": "his"
+            },
+            {
+                "value": "her"
+            }
+        ],
+        "answer": "her",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5030,
+        "question": "We ______our holiday yet",
+        "plans": [
+            {
+                "value": "plan"
+            },
+            {
+                "value": "plans"
+            },
+            {
+                "value": "haven’t planed"
+            }
+        ],
+        "answer": "haven’t planed",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5031,
+        "question": "My brother is taller ______ my father",
+        "plans": [
+            {
+                "value": "than"
+            },
+            {
+                "value": "like"
+            },
+            {
+                "value": "then"
+            }
+        ],
+        "answer": "than",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5032,
+        "question": "After Monday is______",
+        "plans": [
+            {
+                "value": "Tuesday"
+            },
+            {
+                "value": "Friday"
+            },
+            {
+                "value": "Saturday"
+            }
+        ],
+        "answer": "Tuesday",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5033,
+        "question": "How____ is the new staff? He is 27 years old.",
+        "plans": [
+            {
+                "value": "tall"
+            },
+            {
+                "value": "far"
+            },
+            {
+                "value": "old"
+            }
+        ],
+        "answer": "old",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5034,
+        "question": "What do you do in your _____time? I usually read book.",
+        "plans": [
+            {
+                "value": "the"
+            },
+            {
+                "value": "good"
+            },
+            {
+                "value": "free"
+            }
+        ],
+        "answer": "free",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5035,
+        "question": "He plays tennis three ____ a week",
+        "plans": [
+            {
+                "value": "times"
+            },
+            {
+                "value": "ball"
+            },
+            {
+                "value": "month"
+            }
+        ],
+        "answer": "times",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5036,
+        "question": "The capital of Viet Nam",
+        "plans": [
+            {
+                "value": "Ha Noi"
+            },
+            {
+                "value": "Hai Phong"
+            },
+            {
+                "value": "Da Nang"
+            }
+        ],
+        "answer": "Ha Noi",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5037,
+        "question": "How ____ is Red River? It’s about 1200 kilometers",
+        "plans": [
+            {
+                "value": "old"
+            },
+            {
+                "value": "long"
+            },
+            {
+                "value": "much"
+            }
+        ],
+        "answer": "long",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5038,
+        "question": "What is NOT fruit?",
+        "plans": [
+            {
+                "value": "an apple"
+            },
+            {
+                "value": "a banana"
+            },
+            {
+                "value": "a telephone"
+            }
+        ],
+        "answer": "a telephone",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5039,
+        "question": "Be quiet! The student’s ________ test.",
+        "plans": [
+            {
+                "value": "doing"
+            },
+            {
+                "value": "are doing"
+            },
+            {
+                "value": "do"
+            }
+        ],
+        "answer": "are doing",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5040,
+        "question": "We have breakfast _____ 6 am",
+        "plans": [
+            {
+                "value": "in"
+            },
+            {
+                "value": "the"
+            },
+            {
+                "value": "at"
+            }
+        ],
+        "answer": "at",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5041,
+        "question": "Which grade is your son in?",
+        "plans": [
+            {
+                "value": "He is in grade 5."
+            },
+            {
+                "value": "He is smart ."
+            },
+            {
+                "value": "He dosen’t like soccer. His favorite sport is basketball."
+            }
+        ],
+        "answer": "He is in grade 5.",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5042,
+        "question": "How much time do you spend each day on Facebook?",
+        "plans": [
+            {
+                "value": "I fotgot his phone number"
+            },
+            {
+                "value": "About 1 hour"
+            },
+            {
+                "value": "Don’t smoking here, please!"
+            }
+        ],
+        "answer": "About 1 hour",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5043,
+        "question": "What _____ is your new car ? It’s red.",
+        "plans": [
+            {
+                "value": "brand"
+            },
+            {
+                "value": "type"
+            },
+            {
+                "value": "color"
+            }
+        ],
+        "answer": "color",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5044,
+        "question": "Which symbol is the logo of Viet Nam Airlines ?",
+        "plans": [
+            {
+                "value": "helicopter"
+            },
+            {
+                "value": "dragon"
+            },
+            {
+                "value": "lotus flower"
+            }
+        ],
+        "answer": "lotus flower",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5045,
+        "question": "Tranning room is _____ 7th floor",
+        "plans": [
+            {
+                "value": "when"
+            },
+            {
+                "value": "on"
+            },
+            {
+                "value": "about"
+            }
+        ],
+        "answer": "on",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5046,
+        "question": "How much is the hat? _____ quite expensive",
+        "plans": [
+            {
+                "value": "They are"
+            },
+            {
+                "value": "They is"
+            },
+            {
+                "value": "It’s"
+            }
+        ],
+        "answer": "It’s",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5047,
+        "question": "How _____ people are there in Logictic Department?",
+        "plans": [
+            {
+                "value": "many"
+            },
+            {
+                "value": "much"
+            },
+            {
+                "value": "far"
+            }
+        ],
+        "answer": "many",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5048,
+        "question": "What’s your favorite ____?. I like cake",
+        "plans": [
+            {
+                "value": "drink"
+            },
+            {
+                "value": "color"
+            },
+            {
+                "value": "food"
+            }
+        ],
+        "answer": "food",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5049,
+        "question": "When is Women’s Day?",
+        "plans": [
+            {
+                "value": "8 - Mar"
+            },
+            {
+                "value": "1 - May"
+            },
+            {
+                "value": "26 - Jun"
+            }
+        ],
+        "answer": "8 - Mar",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5050,
+        "question": "I hope the ______ can repair my car quickly",
+        "plans": [
+            {
+                "value": "mechanic"
+            },
+            {
+                "value": "teacher"
+            },
+            {
+                "value": "football player "
+            }
+        ],
+        "answer": "mechanic",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5051,
+        "question": "Where did you go last weekend? I______ to Nha Trang with my family.",
+        "plans": [
+            {
+                "value": "traveling"
+            },
+            {
+                "value": "to travel"
+            },
+            {
+                "value": "traveled"
+            }
+        ],
+        "answer": "traveled",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5052,
+        "question": "I don’t like coffee ___ beer. They are unhealthy.",
+        "plans": [
+            {
+                "value": "so"
+            },
+            {
+                "value": "on"
+            },
+            {
+                "value": "and"
+            }
+        ],
+        "answer": "and",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5053,
+        "question": "Yen is 10 years old. She is twice as old as Linh- her sister. How old is Linh?",
+        "plans": [
+            {
+                "value": "5 years old"
+            },
+            {
+                "value": "7 years old"
+            },
+            {
+                "value": "8 years old"
+            }
+        ],
+        "answer": "5 years old",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5054,
+        "question": "______ is your favorite player? Lionel Messi. He is the best.",
+        "plans": [
+            {
+                "value": "What"
+            },
+            {
+                "value": "Who"
+            },
+            {
+                "value": "Which"
+            }
+        ],
+        "answer": "Who",
+        "selectedAnswer": null
+    },
+    {
+        "id": 5055,
+        "question": "Can you turn ______ the music speaker? It too nosie.",
+        "plans": [
+            {
+                "value": "in"
+            },
+            {
+                "value": "on"
+            },
+            {
+                "value": "down"
+            }
+        ],
+        "answer": "down",
+        "selectedAnswer": null
     }
 ]
